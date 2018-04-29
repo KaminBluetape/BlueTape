f7db775e4b196dba5fd6ad77c62fed27c7f6455b
miebakso <matthew_ariel_wangsit@yahoo.com>
2018-04-29 13:42:07 +0700

    Little Fix

--- www/token.php
+++ www/token.php
@@ -1,13 +1,13 @@
 <form method="post" action="/TranskripRequest/add">
 	
-	<input type="hidden" name="csrf_token" value=<?php echo $_GET['token'] ?> >
+	<!-- <input type="hidden" name="csrf_token" value=<?php echo $_GET['token'] ?> -->
 	
 	<label>Your Name</label>
 	<input type="text" name="requestUsage">
     
     <select name="requestType">
-    	<option value="DPS_ID">DPS Bahasa Indonesia (Seluruh Semester)</option>
-		<option value="DPS_EN">DPS Bahasa Inggris (Seluruh Semester)</option>
+    	<option value="DPS_ID">$1000</option>
+		<option value="DPS_EN">$100</option>
 	</select>
 
 	<button type=submit>EARN $$$ QUICKLY</button>
